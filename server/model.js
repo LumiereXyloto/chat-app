2df43f4496cbdb5a802674431f8f0b9a319ea269
Lumiere Xyloto <740207968@qq.com>
2019-03-23 20:02:24 +0800

    feat: 登录注册基本实现

--- server/model.js
+++ server/model.js
@@ -1,7 +1,30 @@
 const mongoose = require('mongoose')
 // 链接MongoDB
-const DB_URL = 'mongodb://localhost:27017/chat-app'
+const DB_URL = 'mongodb://localhost:27017/imooc-chat'
 mongoose.connect(DB_URL)
-mongoose.connection.on('connected', function(){
-  console.log('mongo connect successfully')
-})
+
+const models = {
+  user: {
+    'user': {type: String, require: true},
+    'pwd': {type: String, require: true},
+    'type': {type: String, require: true},
+    'avatar': {type: String},
+    'desc': {type:String},
+    'title': {type: String},
+    'company': {type: String},
+    'money': {type: String}
+  },
+  chat: {
+
+  }
+}
+
+for(let m in models){
+  mongoose.model(m, new mongoose.Schema(models[m]))
+}
+
+module.exports = {
+  getModel: function(name) {
+    return mongoose.model(name)
+  }
+}
--- src/container/register/register.js
+++ src/container/register/register.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import Logo from '../../component/logo/logo'
-import { List, InputItem, Radio, WhiteSpace,Button } from 'antd-mobile';
+import { List, InputItem, Radio, WhiteSpace,Button } from 'antd-mobile'
+import { connect } from 'react-redux'
+import { register } from '../../redux/user.redux'
+import { Redirect } from 'react-router-dom'
+
+@connect(
+  state => state.user,
+  {register}
+)
 
 class Register extends React.Component{
   constructor (props) {
@@ -19,14 +27,16 @@ class Register extends React.Component{
     })
   }
   handleRegister () {
-    console.log(this.state)
+    this.props.register(this.state)
   }
   render () {
     const RadioItem = Radio.RadioItem
     return (
       <div>
+        {this.props.redirectTo?<Redirect to={this.props.redirectTo}/>:null}
         <Logo></Logo>
         <List>
+          {this.props.msg?<p className="error-msg">{this.props.msg}</p> : null}
           <InputItem
             onChange={v => this.handleChange('user', v)}
           >用户名</InputItem>
